c64a8f59cc2fd7c80730274f1309d95b9ba04fa8
Bruce <gkm85663@gmail.com>
2020-01-06 02:56:42 +0800

    Refactor

--- LINE_Bot/Form1.cs
+++ LINE_Bot/Form1.cs
@@ -12,11 +12,8 @@ namespace LINE_Bot
 
         private void Btn_Send_Message_Click(object sender, EventArgs e)
         {
-            var token = "ufFSRRG5kRRWF7cWQfIXj5dbVHda0mwH8x5vS1OSHxWfCfqXNwvHMqfgXUFyR4Bjt8ACWpSoJwqyj+y0QAy/YB4mrF9+exiJO3YRlCewXgdk6L65H2YgBvNpHPH1ZRqkw7rmqdis2Bh6SEbWaF7X8wdB04t89/1O/w1cDnyilFU=";
-            var adminUserId = "U89e2dae55874fe65fd11d313b89f0a00";
-            var lineBot = new isRock.LineBot.Bot(token);
-
-            lineBot.PushMessage(adminUserId, "Hello handsome man!");
+            var lineBot = new LineBot();
+            lineBot.PushMessage("Hello handsome man!");
         }
     }
 }
---
+++ LINE_Bot/LineBot.cs
@@ -0,0 +1,23 @@
+﻿using isRock.LineBot;
+
+namespace LINE_Bot
+{
+    public class LineBot
+    {
+        private readonly string _adminUserId;
+        private static string _token;
+        private static Bot Bot { get; set; }
+
+        public LineBot()
+        {
+            _token = "ufFSRRG5kRRWF7cWQfIXj5dbVHda0mwH8x5vS1OSHxWfCfqXNwvHMqfgXUFyR4Bjt8ACWpSoJwqyj+y0QAy/YB4mrF9+exiJO3YRlCewXgdk6L65H2YgBvNpHPH1ZRqkw7rmqdis2Bh6SEbWaF7X8wdB04t89/1O/w1cDnyilFU=";
+            _adminUserId = "U89e2dae55874fe65fd11d313b89f0a00";
+            Bot = new Bot(_token);
+        }
+
+        public void PushMessage(string message)
+        {
+            Bot.PushMessage(_adminUserId, message);
+        }
+    }
+}
